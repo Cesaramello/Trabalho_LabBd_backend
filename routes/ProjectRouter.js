75cc369e8517b7a2efc6465162f919ad75e5b3cc
Elmo Junior Ficagna <elmojrf@gmail.com>
2019-11-19 19:18:15 -0400

    Crud básico de Projetos finalizado

--- routes/ProjectRouter.js
+++ routes/ProjectRouter.js
@@ -16,21 +16,91 @@ const resourceName = '/project';
 server.get(resourceName, validateToken, (request, response, next) => {
 
     services.getAll()
-        .then(events => {
-            console.log(events);
-            response.send(HttpStatus.OK, events ? events : []);
+        .then(projects => {
+            console.log(projects);
+            response.send(HttpStatus.OK, projects ? projects : []);
         })
         .catch(err => {
             console.error(err);
-            response.send(HttpStatus.SERVICE_UNAVAILABLE, errr);
+            response.send(HttpStatus.SERVICE_UNAVAILABLE, err);
         })
+
+    next();
+});
+
+server.get(resourceName + '/:projectId', validateToken, (request, response, next) => {
+
+    const {
+        projectId,
+    } = request.params;
+
+    const project = {
+        id: projectId
+    };
+
+    services.get(project)
+        .then(project => {
+            console.log(project);
+            response.send(HttpStatus.OK, project);
+        })
+        .catch(err => {
+            console.error(err);
+            response.send(HttpStatus.NOT_FOUND, err);
+        });
 
     next();
 });
 
 server.post(resourceName, validateToken, (request, response, next) => {
 
-    
+    const userId = request.header('userId');
+
+    const {
+        projectName,
+        projectDesc
+    } = request.params;
+
+    const project = {
+        name: projectName || null,
+        desc: projectDesc || null,
+        ProjectOwnerId: userId
+    }
+
+    services.create(project)
+        .then(createdProject => {
+            console.log(createdProject);
+            response.send(HttpStatus.OK, createdProject);
+        })
+        .catch(err => {
+            console.error(err);
+            response.send(HttpStatus.UNPROCESSABLE_ENTITY, err);
+        })
+
+    next();
+});
+
+server.del(resourceName + '/:projectId', validateToken, (request, response, next) => {
+
+    const userId = request.header('userId');
+
+    const {
+        projectId,
+    } = request.params;
+
+    const project = {
+        id: projectId,
+        ProjectOwnerId: userId
+    }
+
+    services.delete(project)
+        .then(deletedProject => {
+            console.log(deletedProject);
+            response.send(HttpStatus.OK, deletedProject);
+        })
+        .catch(err => {
+            console.error(err);
+            response.send(HttpStatus.UNPROCESSABLE_ENTITY, err);
+        })
 
     next();
 });
--- services/ProjectServices.js
+++ services/ProjectServices.js
@@ -15,4 +15,46 @@ ProjectServices.getAll = () => new Promise((resolve, reject) => {
         });
 });
 
+ProjectServices.get = (project) => new Promise((resolve, reject) => {
+    Project.findOne({
+            where: {
+                id: project.id
+            }
+        })
+        .then((projectFound) => {
+            if (projectFound) {
+                resolve(projectFound);
+            } else {
+                reject("Não foi possível encontrar o projeto.");
+            }
+        })
+        .catch(err => {
+            reject(err);
+        });
+});
+
+ProjectServices.create = (project) => new Promise((resolve, reject) => {
+    Project.create(project)
+        .then((createdProject) => resolve(createdProject))
+        .catch(err => {
+            reject(err);
+        });
+});
+
+
+ProjectServices.delete = (project) => new Promise((resolve, reject) => {
+    ProjectServices.get(project)
+        .then((projectFound) => {
+            if (projectFound.ProjectOwnerId == project.ProjectOwnerId) {
+                projectFound.destroy()
+                    .then((removedProject) => resolve(removedProject));
+            } else {
+                reject("Você não possui permissão para excluir este projeto.");
+            }
+        })
+        .catch(err => {
+            reject(err);
+        });
+});
+
 module.exports = ProjectServices;
